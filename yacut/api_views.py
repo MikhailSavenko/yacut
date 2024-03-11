382fe5169ee69dce06b45c82857b9ca41291f0a2
Михаил Савенко <mi47sav@gmail.com>
2024-03-11 20:09:53 +0300

    Rew api_views поправлен

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -2,10 +2,11 @@ from flask import jsonify, request
 from sqlalchemy.exc import SQLAlchemyError
 
 from . import app, db
-from .error_handlers import InvalidAPIUsage
+from .error_handlers import InvalidAPIUsage, NotFoundException
 from .models import URLMap
 from .validators import duplicate_validator, validator_custom_id
 from .views import random_short_url
+from http import HTTPStatus
 
 
 def create_new_object_api(data):
@@ -29,24 +30,19 @@ def create_id():
         raise InvalidAPIUsage('Отсутствует тело запроса')
     if 'url' not in data:
         raise InvalidAPIUsage('\"url\" является обязательным полем!')
-    if data.get('url') is None or data.get('url') == '':
+    if not data.get('url'):
         raise InvalidAPIUsage('Отсутствует тело запроса')
-    if (
-        'custom_id' in data
-        and data.get('custom_id') is not None
-        and data.get('custom_id') != ''
-    ):
+    if data.get('custom_id'):
         short = data.get('custom_id')
         validator_custom_id(data.get('custom_id'))
         duplicate_validator(short)
         short_url = create_new_object_api(data)
-        return jsonify(short_url.to_dict()), 201
-    else:
-        short = random_short_url()
-        duplicate_validator(short)
-        data['custom_id'] = short
-        short_url = create_new_object_api(data)
-        return jsonify(short_url.to_dict()), 201
+        return jsonify(short_url.to_dict()), HTTPStatus.CREATED
+    short = random_short_url()
+    duplicate_validator(short)
+    data['custom_id'] = short
+    short_url = create_new_object_api(data)
+    return jsonify(short_url.to_dict()), HTTPStatus.CREATED
 
 
 @app.route('/api/id/<string:short_id>/', methods=['GET'])
@@ -54,6 +50,5 @@ def get_url(short_id):
     url = URLMap.query.filter_by(short=short_id).first()
     if url:
         url = url.original
-        return jsonify({"url": url}), 200
-    else:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
+        return jsonify({'url': url}), HTTPStatus.OK
+    raise NotFoundException('Указанный id не найден')
--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,10 +1,10 @@
 from flask import jsonify, render_template
-
+from http import HTTPStatus
 from . import app, db
 
 
 class InvalidAPIUsage(Exception):
-    status_code = 400
+    status_code = HTTPStatus.BAD_REQUEST
 
     def __init__(self, message, status_code=None) -> None:
         super().__init__()
@@ -16,6 +16,24 @@ class InvalidAPIUsage(Exception):
         return dict(message=self.message)
 
 
+class NotFoundException(Exception):
+    status_code = HTTPStatus.NOT_FOUND
+
+    def __init__(self, message, status_code=None) -> None:
+        super().__init__()
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+
+    def to_dict(self):
+        return dict(message=self.message)
+
+
+@app.errorhandler(NotFoundException)
+def invalid_api_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+
 @app.errorhandler(InvalidAPIUsage)
 def invalid_api_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -23,10 +41,10 @@ def invalid_api_usage(error):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html'), HTTPStatus.NOT_FOUND
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('500.html'), HTTPStatus.INTERNAL_SERVER_ERROR
